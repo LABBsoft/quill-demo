19e093e5f9c032998d22cbf951aaad03be496195
Luciano Abbott <luciano.abbott@intelliware.com>
2020-08-07 12:18:18 -0400

    LA: Quill demo for RBC

---
+++ src/editor/editor.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core'
+import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill'
+import Quill from 'quill'
+
+@Component({
+  selector: 'app-editor',
+  templateUrl: './editor.component.html'
+})
+export class EditorComponent {
+  blurred = false
+  focused = false
+
+  created(event: Quill) {
+    // tslint:disable-next-line:no-console
+    console.log('editor-created', event)
+  }
+
+  changedEditor(event: EditorChangeContent | EditorChangeSelection) {
+    // tslint:disable-next-line:no-console
+    console.log('editor-change', event)
+  }
+
+  focus($event) {
+    // tslint:disable-next-line:no-console
+    console.log('focus', $event)
+    this.focused = true
+    this.blurred = false
+  }
+
+  blur($event) {
+    // tslint:disable-next-line:no-console
+    console.log('blur', $event)
+    this.focused = false
+    this.blurred = true
+  }
+}
